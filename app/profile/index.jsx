cc69fdea160802fdbea05e221259af6d87b27755
r021n <rozinabdul25@gmail.com>
2024-12-06 17:28:45 +0700

    fixing profile menu

--- app/profile/index.jsx
+++ app/profile/index.jsx
@@ -48,6 +48,7 @@ const ProfilePengembang = () => {
             flex: 1,
             // backgroundColor: "yellow",
             marginHorizontal: 120,
+            marginTop: -20,
           }}
         >
           <ImageBackground
@@ -58,6 +59,52 @@ const ProfilePengembang = () => {
               justifyContent: "center",
             }}
           ></ImageBackground>
+        </View>
+
+        <View
+          style={{
+            position: "absolute",
+            display: "flex",
+            flexDirection: "row",
+            // backgroundColor: "yellow",
+            width: "100%",
+            justifyContent: "center",
+            bottom: 40,
+          }}
+        >
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              //   backgroundColor: "pink",
+              width: "100%",
+              paddingHorizontal: 30,
+            }}
+          >
+            <TouchableOpacity>
+              <Image
+                source={require("../../assets/icons/swipe_left.png")}
+                resizeMode="contain"
+                style={{
+                  width: 100,
+                  height: 30,
+                  // backgroundColor: "lime"
+                }}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                source={require("../../assets/icons/swipe_right.png")}
+                resizeMode="contain"
+                style={{
+                  width: 100,
+                  height: 30,
+                  //  backgroundColor: "lime"
+                }}
+              />
+            </TouchableOpacity>
+          </View>
         </View>
       </ImageBackground>
     </View>
