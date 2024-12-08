2af126b7b9d538a2387872def6170ef11a7b186b
r021n <rozinabdul25@gmail.com>
2024-12-08 22:45:48 +0700

    make materi layout

--- app/materi/index.jsx
+++ app/materi/index.jsx
@@ -1,10 +1,55 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import React from "react";
+import Header from "../../components/header";
 
 const Materi = () => {
   return (
-    <View>
-      <Text>Materi</Text>
+    <View style={{ flex: 1 }}>
+      <ImageBackground
+        source={require("../../assets/bg/blue_bg.png")}
+        resizeMode="cover"
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <View style={{ flex: 1 }}>
+          <Header />
+          <View
+            style={{
+              flex: 1,
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+            }}
+          >
+            <TouchableOpacity>
+              <Image
+                source={require("../../assets/images/penyebab_menu.png")}
+                resizeMode="contain"
+                style={{
+                  width: 300,
+                  height: 170,
+                }}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                source={require("../../assets/images/usaha_menu.png")}
+                resizeMode="contain"
+                style={{
+                  width: 300,
+                  height: 170,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
