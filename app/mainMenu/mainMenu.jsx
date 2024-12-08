a1614001a5a69a0cbef62b603c23874763a82ae2
r021n <rozinabdul25@gmail.com>
2024-12-08 22:43:17 +0700

    adding leave confirmation feature

--- app/mainMenu/mainMenu.jsx
+++ app/mainMenu/mainMenu.jsx
@@ -4,12 +4,42 @@ import {
   ImageBackground,
   SafeAreaView,
   StatusBar,
+  Alert,
+  BackHandler,
 } from "react-native";
-import React from "react";
+import { useEffect } from "react";
 import Header from "../../components/header";
 import MenuList from "../../components/menuList";
+import { useIsFocused } from "@react-navigation/native";
 
 const MainMenu = () => {
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if (isFocused) {
+      const backAction = () => {
+        Alert.alert(
+          "Notifikasi",
+          "Apakah kamu ingin keluar dari aplikasi ini?",
+          [
+            {
+              text: "Batal",
+              onPress: () => null,
+              style: "cancel",
+            },
+            { text: "Ya", onPress: () => BackHandler.exitApp() },
+          ]
+        );
+        return true;
+      };
+
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        backAction
+      );
+
+      return () => backHandler.remove();
+    }
+  }, [isFocused]);
   return (
     <SafeAreaView
       style={{
