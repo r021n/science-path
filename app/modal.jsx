b7085510101ce865f0a814f932fe2e16750525a2
r021n <rozinabdul25@gmail.com>
2024-12-13 15:36:51 +0700

    delete console.log and integrate with spreadsheet

--- app/modal.jsx
+++ app/modal.jsx
@@ -4,54 +4,85 @@ import {
   TouchableOpacity,
   ImageBackground,
   Image,
+  ActivityIndicator,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useForm } from "../context/answerContext";
-import { doc, setDoc, collection, getDocs } from "firebase/firestore";
-import { db } from "../configs/firebaseConfig";
 
 const Modal = () => {
   const router = useRouter();
 
   const { userData, setUserData } = useForm();
   const { keyAnswer, type } = useLocalSearchParams();
+  const [loading, setLoading] = useState(false);
 
   let answerList = userData.userAnswers;
   const keyAnswerData = keyAnswer.split(",").map(Number);
   let score = null;
   let totalSame = 0;
 
+  const convertNum = (num) => {
+    switch (num) {
+      case 0:
+        return "A";
+        break;
+      case 1:
+        return "B";
+        break;
+      case 2:
+        return "C";
+        break;
+      case 3:
+        return "D";
+        break;
+
+      default:
+        break;
+    }
+  };
+
   const getScore = async () => {
+    setLoading(true);
+
     for (let i = 0; i < answerList.length; i++) {
       if (answerList[i] == keyAnswerData[i]) {
         totalSame++;
       }
     }
 
-    score = (totalSame / answerList.length) * 100;
+    score = (totalSame / keyAnswerData.length) * 100;
 
-    console.log(userData.name);
-    console.log(keyAnswerData);
-    console.log(answerList);
-    console.log(totalSame);
-    // console.log(score);
+    console.log("nama user: ", userData.name);
+    console.log("Kunci jawaban: ", keyAnswerData);
+    console.log("jawaban siswa: ", answerList);
+    console.log("Jumlah benar: ", totalSame);
+    console.log("jumlah salah: ", keyAnswerData.length - totalSame);
+    console.log("nilai yang didapat: ", score);
 
-    answerList = [];
+    // ==============> Store data to spreedsheet
 
-    // ==============> send data to firebase
-    // userData.score = score;
-    // await setDoc(doc(db, type, userData.name), userData);
-    // await setUserData({});
+    const postestScript = process.env.POSTEST_KEY;
+    const pretestScript = process.env.PRETEST_KEY;
 
-    // ==============> read data from firebase
-    // const querySnapshot = await getDocs(collection(db, type));
-    // querySnapshot.forEach((doc) => {
-    //   console.log(doc.id, " => ", doc.data().userAnswers[0]);
-    // });
-
-    // setUserData({});
-    // router.dismissTo({ pathname: "/soal" });
+    const url = type == "pretest" ? pretestScript : postestScript;
+    fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body:
+        `Nama=${userData.name}&Kelas=${userData.class}` +
+        answerList
+          .map((answer, index) => `&Jawaban_${index + 1}=${convertNum(answer)}`)
+          .join("") +
+        `&Total_benar=${totalSame}&Nilai=${score}`,
+    })
+      .then((res) => res.text())
+      .then((data) => {
+        // alert(data);
+        setUserData({});
+        router.dismissTo({ pathname: "/soal" });
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -68,37 +99,42 @@ const Modal = () => {
             Yakin ingin menyelesaikan {type} ini?
           </Text>
         </View>
-        <View
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 20,
-          }}
-        >
-          <TouchableOpacity onPress={() => router.back()}>
-            <Image
-              source={require("../assets/icons/batal_btn.png")}
-              style={{
-                height: 30,
-                width: 120,
-              }}
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
 
-          <TouchableOpacity onPress={getScore}>
-            <Image
-              source={require("../assets/icons/yakin_btn.png")}
-              style={{
-                height: 30,
-                width: 120,
-              }}
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-        </View>
+        {loading ? (
+          <ActivityIndicator size={40} />
+        ) : (
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: 20,
+            }}
+          >
+            <TouchableOpacity onPress={() => router.back()}>
+              <Image
+                source={require("../assets/icons/batal_btn.png")}
+                style={{
+                  height: 30,
+                  width: 120,
+                }}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={getScore}>
+              <Image
+                source={require("../assets/icons/yakin_btn.png")}
+                style={{
+                  height: 30,
+                  width: 120,
+                }}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          </View>
+        )}
       </ImageBackground>
     </View>
   );
