fa7c0e51858ed1502919d1a63a24338978e2b9de
r021n <rozinabdul25@gmail.com>
2025-02-15 15:07:08 +0700

    upgrade dependencies and change materi code

--- components/materi/materi1/materi1.jsx
+++ components/materi/materi1/materi1.jsx
@@ -5,7 +5,13 @@ import Sub2 from "./subMateri/sub2";
 import Sub3 from "./subMateri/sub3";
 
 const Materi1 = ({ page }) => {
-  return <View>{page == 1 ? <Sub1 /> : page == 2 ? <Sub2 /> : <Sub3 />}</View>;
+  const materiList = [Sub1, Sub2, Sub3];
+  const Materi = materiList[page - 1];
+  return (
+    <View>
+      <Materi />
+    </View>
+  );
 };
 
 export default Materi1;
